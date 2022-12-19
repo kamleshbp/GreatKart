8e07b1ddb56a7d8f01717286a780d96ca2d016de
Kamlesh Prajapati <49552163+kamleshbp@users.noreply.github.com>
2022-12-19 23:15:49 +0530

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -141,6 +141,6 @@ MESSAGE_TAGS = {
 # SMTP settings for sending email
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kbp040506@gmail.com'
-EMAIL_HOST_PASSWORD = 'xtiagwhdtjwxgavj'
+EMAIL_HOST_PASSWORD = '****************'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
