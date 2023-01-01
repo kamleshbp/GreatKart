8d4fdf45e3a6f3ce51d302a8fb6061cab146e4ce
Kamlesh Prajapati <kamleshprajapati0405@gmail.com>
2023-01-01 18:41:50 +0530

    Seperated configurations from the code

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9xfjx=qhk0fw(4@g&516ce+1k*915a=$1qdjk-a@&o_q#fsd@u'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool) # True
 
 ALLOWED_HOSTS = []
 
@@ -140,8 +141,8 @@ MESSAGE_TAGS = {
 
 
 # SMTP settings for sending email
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'kbp040506@gmail.com'
-EMAIL_HOST_PASSWORD = '****************'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
