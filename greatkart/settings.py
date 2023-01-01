4c9f3bbbf08c2a7ebd8b606cd663620f97a75b81
Kamlesh Prajapati <kamleshprajapati0405@gmail.com>
2023-01-01 19:34:45 +0530

    Added session timeout

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -54,7 +54,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
+
+# settings for session expiration
+SESSION_EXPIRE_SECONDS = 3600  # 1 hour
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = 'accounts/login/'
 
 ROOT_URLCONF = 'greatkart.urls'
 
